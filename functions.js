8f587d27dfe728c865111300b79dd75f71a4582b
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-01 17:07:15 +0000

    add Function.prototype.call()

--- functions.js
+++ functions.js
@@ -1,6 +1,9 @@
 const functions = {
-    funcLength: func => func.length,
-    funcName: func => func.name,
+  funcLength: (func) => func.length,
+  funcName: (func) => func.name,
+  funcCall: function (input1, input2) {
+    return`Hello, ${this.firstName} ${this.lastName}. Your access Level is ${this.accessLevel}. Profile active? ${input1}. Any issues to submit? ${input2}.`
+  },
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -2,10 +2,22 @@ const { expect } = require('@jest/globals')
 const functions = require('./functions')
 
 test('should return the number of the arguments expected by the passed function', () => {
-    expect(functions.funcLength((a, b, c) => a + b + c)).toEqual(3)
+  expect(functions.funcLength((a, b, c) => a + b + c)).toEqual(3)
 })
 
 test('should return the name of the passed function', () => {
-    const addTwoNumbers = (a, b) => a + b 
-    expect(functions.funcName(addTwoNumbers)).toEqual('addTwoNumbers')
+  const addTwoNumbers = (a, b) => a + b
+  expect(functions.funcName(addTwoNumbers)).toEqual('addTwoNumbers')
+})
+
+test('should call the passed function with the provided this value and individual arguments', () => {
+  const obj = {
+    firstName: 'Dale',
+    lastName: 'Cooper',
+    email: 'dale@twinpeaks.co.uk',
+    accessLevel: 3,
+  }
+  expectedOutput =
+    'Hello, Dale Cooper. Your access Level is 3. Profile active? true. Any issues to submit? false.'
+  expect(functions.funcCall.call(obj, true, false)).toEqual(expectedOutput)
 })
